a7b574f365474104e6b04ef1c04d0f02a9ab801c
jordypaucar513-blip <jordypaucar513@gmail.com>
2026-06-19 08:08:24 -0500

    Designacion del inicio de la app

---
+++ app/src/main/java/com/example/stockpro/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.stockpro
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.example.stockpro.navigation.AppNavigation
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        setContent {
+
+            AppNavigation()
+        }
+    }
+}
